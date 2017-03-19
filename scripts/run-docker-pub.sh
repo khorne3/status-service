8ec9672ab4e34113343744af8a95a37bd84b3554
Anthony Bond <ajbond2005@gmail.com>
2017-03-19 12:02:12 -0400

    Updated deploy to docker hub

--- scripts/run-docker-pub.sh
+++ scripts/run-docker-pub.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -ev
 scode=`curl -s -o /dev/null -w "%{http_code}" http://localhost/api/status`
-if [ $scode -ne 200 ]
+if [ $scode != 200 ]
 then
   echo "Status Code: $scode"
   exit 1
+else
+  echo "Container Online: $scode"
 fi
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]
 then
   docker login -u="$DOCKERU" -p="$DOCKERP";
   docker push $DOCKERU/service-status:latest;
 fi
-
